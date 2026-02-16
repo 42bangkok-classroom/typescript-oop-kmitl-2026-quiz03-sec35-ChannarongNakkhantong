c7059386eeebb66b428e2f699b7825ecdd60c35b
Channarong <channarongnakkhantong@gmail.com>
2026-02-16 09:44:43 +0700

    02

--- src/practicals/p02/person.ts
+++ src/practicals/p02/person.ts
@@ -1,1 +1,35 @@
-export class Person {}
+export class Person {
+    static COUNTRY = 'thailand';
+    firstname?:string;
+    lastname?:string;
+    private age?:number;
+    constructor(firstname?:string,lastname?:string){
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+    setAge(age:number){
+        return this.age = age;
+    }
+    getAge(){
+        return this.age;
+    }
+    getFullName(){
+        return `${this.firstname} ${this.lastname}`;
+    }
+}
+const person = new Person()
+
+person.firstname = 'John'
+person.lastname = 'Doe'
+person.setAge(25)
+
+console.log(person.getFullName())
+// John Doe
+
+console.log(person.getAge())
+// 25
+
+console.log(Person.COUNTRY)
+// Thailand   (หรือค่าคงที่ที่กำหนดเอง)
+
+
