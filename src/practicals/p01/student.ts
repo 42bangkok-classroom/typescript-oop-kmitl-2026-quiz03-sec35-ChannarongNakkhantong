51560a6964b2707455683c4090ccf5ca7774e7c6
Channarong <channarongnakkhantong@gmail.com>
2026-03-25 01:20:41 +0700

    test

--- src/practicals/p01/student.ts
+++ src/practicals/p01/student.ts
@@ -1,15 +1,14 @@
 export class Student {
-    firstname?:string;
-    lastname?:string;
-    constructor(firstname?:string,lastname?:string){
+    firstname: string;
+    lastname: string;
+
+    constructor(firstname: string, lastname: string) {
         this.firstname = firstname;
         this.lastname = lastname;
     }
-    printName(){
+    printName(): void {
         console.log(`${this.firstname} ${this.lastname}`);
     }
 }
-const student = new Student()
-student.firstname = 'John'
-student.lastname = 'Doe'
-student.printName()
+
+
