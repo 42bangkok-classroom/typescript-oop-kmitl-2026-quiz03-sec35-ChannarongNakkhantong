fcc8338aa0d01947d7a1a57db59ae148857bc616
Channarong <channarongnakkhantong@gmail.com>
2026-03-25 03:21:49 +0700

    testp03

--- src/practicals/p03/playstation.ts
+++ src/practicals/p03/playstation.ts
@@ -1,5 +1,17 @@
 import { Product } from './product'
 
 export class Playstation extends Product {
+  generation: number;
+  constructor(name: string, generation: number, price: number = 0) {
+    super(name, price)
+    this.generation = generation;
+  }
+  getProfile() {
+    return `${this.name} (Gen ${this.generation})`
+  }
+  getDiscountPrice() {
+    //หาส่วนลดก่อน
+    this.price = this.price - (Product.DISCOUNT_PERCENT / 100 * this.price);
+  }
   // TODO: implement class properties, constructor with super(...), and methods
 }
--- src/practicals/p03/product.ts
+++ src/practicals/p03/product.ts
@@ -1,2 +1,16 @@
-export class Product {}
+export class Product {
+    readonly name: string;
+    protected price: number;
+    static readonly DISCOUNT_PERCENT = 10;
+    constructor(name: string = "", price: number = 0) {
+        this.name = name;
+        this.price = price;
+    }
+    getPrice() {
+        return this.price;
+    }
+    setPrice(price: number) {
+        this.price = price;
+    }
+}
 
